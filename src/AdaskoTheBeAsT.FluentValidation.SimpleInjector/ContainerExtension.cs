ae9c05227bcf32eb5e3477436cbacc6d3a471fb2
Adam Pluciński <adaskothebeast@gmail.com>
2024-01-27 13:37:25 +0100

    removed non used using

--- src/AdaskoTheBeAsT.FluentValidation.SimpleInjector/ContainerExtension.cs
+++ src/AdaskoTheBeAsT.FluentValidation.SimpleInjector/ContainerExtension.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Reflection;
 using FluentValidation;
 using SimpleInjector;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace AdaskoTheBeAsT.FluentValidation.SimpleInjector
 {
