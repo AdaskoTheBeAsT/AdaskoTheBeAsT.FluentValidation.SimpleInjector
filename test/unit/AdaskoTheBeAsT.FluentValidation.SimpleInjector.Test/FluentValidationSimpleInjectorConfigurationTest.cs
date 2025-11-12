bd65d493ca80f2564ab0f6d6c80409cab5ffb66d
Adam Pluciński <adaskothebeast@gmail.com>
2025-11-12 20:20:38 +0100

    upgrade to latest versions FluentValidation and .NET 10

--- test/unit/AdaskoTheBeAsT.FluentValidation.SimpleInjector.Test/FluentValidationSimpleInjectorConfigurationTest.cs
+++ test/unit/AdaskoTheBeAsT.FluentValidation.SimpleInjector.Test/FluentValidationSimpleInjectorConfigurationTest.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using FluentAssertions;
-using FluentAssertions.Execution;
+using AwesomeAssertions;
+using AwesomeAssertions.Execution;
 using SimpleInjector;
 using Xunit;
 
